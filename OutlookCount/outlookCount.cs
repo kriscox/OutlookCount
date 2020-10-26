f70fb840f4b557d60546a19dadf350060630b3e0
Kris Cox <kcox@parking.brussels>
2020-10-26 11:52:59 +0100

    Removed uneccesaire imports

--- OutlookCount/outlookCount.cs
+++ OutlookCount/outlookCount.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.Office.Interop.Outlook;
-using Microsoft.VisualBasic.FileIO;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using Microsoft.Office.Interop.Outlook;
+using Microsoft.VisualBasic.FileIO;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using Outlook = Microsoft.Office.Interop.Outlook;
+
 
 namespace OutlookCount
 {
@@ -20,47 +20,17 @@ namespace OutlookCount
         }
     }
 
-    class Program
+    static class outlookCount
     {
-        static string mailBox = null;
-        static Outlook._Application oApp = null;
-        static int totalSentMails = 0;
-        static int totalReceivedMails = 0;
-        static string restrictDate = null;
-        static readonly List<CountAgent> CountAgents = new List<CountAgent> { };
+        private static Outlook._Application oApp = null;
+        private static int totalSentMails = 0;
+        private static int totalReceivedMails = 0;
+        private static readonly List<CountAgent> CountAgents = new List<CountAgent> { };
+        public string MailBox { get; set; }
+        public string RestrictDate { get; set; }
 
-        static int Main(string[] args)
+        public outlookCount()
         {
-            // Test if input arguments were supplied.
-            if ((args.Length != 4) && (args.Length != 6))
-            {
-                ErrorParameters();
-                return 1;
-            }
-
-            // Read first two arguments
-            int returnValue = ReadTwoArguments(args.Take(2).ToArray());
-            if (returnValue != 0)
-            {
-                return returnValue;
-            }
-
-            // Read the next two arguments
-            returnValue = ReadTwoArguments(args.Skip(2).Take(2).ToArray());
-            if (returnValue != 0)
-            {
-                return returnValue;
-            }
-
-            if (args.Length == 6)
-            {
-                returnValue = ReadTwoArguments(args.Skip(4).Take(2).ToArray());
-                if (returnValue != 0)
-                {
-                    return returnValue;
-                }
-            }
-
             // Start outlook
             try
             {
@@ -71,122 +41,27 @@ namespace OutlookCount
             catch (System.Exception e)
             {
                 Console.WriteLine("{0} Exception caught while opening Outlook: ", e);
-                return 7;
             }
+        }
 
-            // Set daterange to count if not given by parameters
-            if (restrictDate == null) DateRange();
-
+        public void Process()
+        {
             //Count mail
             CountMailbox();
 
             // send results
             ShowResults();
-
-            return 0;
-        }
-
-        /// <summary>
-        ///     DateRange 
-        ///     
-        /// DateRange defines the range in time to search
-        /// 
-        /// </summary>
-        private static void DateRange(DateTime? givenDate = null)
-        {
-            // get the begin and end of yesterday
-
-            DateTime dtRestrict = givenDate == null ? DateTime.Now.AddDays(-1) : (DateTime)givenDate;
-            DateTime dtEnd = new DateTime(dtRestrict.Year, dtRestrict.Month, dtRestrict.Day, 23, 59, 59, 999);
-            DateTime dtStart = new DateTime(dtRestrict.Year, dtRestrict.Month, dtRestrict.Day, 0, 0, 0, 0);
-            restrictDate = " [received] >= \"" + dtStart.ToString("g") + "\" AND [received] <= \"" + dtEnd.ToString("g") + "\"";
-        }
-
-        /// <summary>
-        ///     ErrorParameters
-        ///     
-        /// Write errormessge for parameters
-        /// 
-        /// </summary>
-        static void ErrorParameters()
-        {
-            Console.WriteLine("Please enter all obligatory arguments.");
-            Console.WriteLine("Usage: outlookcount -e <mailbox name> -c <csv-file containing agent codes> [-d <date>]");
-        }
-
-        /// <summary>
-        /// Read two arguments from a list and check their validity, if valid, put them in the necessairy objects
-        /// </summary>
-        /// <param name="args">list of 2 parameters</param>
-        /// <returns>
-        /// <list>
-        ///     <item>0:  Succesfull </item>
-        ///     <item>1:  Wrong code in parameter</item>
-        ///     <item>5:  Wrong date format</item>
-        ///     <item>6:  Error reading codes from file</item>
-        ///     <item>7:  Wrong filename</item>
-        /// </list>
-        /// </returns>
-        private static int ReadTwoArguments(string[] args)
-        {
-
-            // Read first argument
-            if (args[0] == "-e")
-            {
-                // read mailbox name 
-                mailBox = args[1];
-            } // first argument Agent Codes
-            else if (args[0] == "-c")
-            {
-                // read the agent codes from file
-                int returnvalue = ReadCodes(args[1]);
-                if (0 != returnvalue)
-                {
-                    return returnvalue;
-                }
-                if (CountAgents.Count() <= 0)
-                {
-                    Console.WriteLine("Agent codes are not read. Error in file format?");
-                    return 6;
-                }
-            } // wrong Arguments, write error message and quit
-            else if (args[0] == "-d")
-            {
-
-                DateTime givenDate;
-                // Read the date
-                try
-                {
-                    givenDate = DateTime.Parse(args[1]);
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine("Wrong date format");
-                    Console.WriteLine(e.Message);
-                    return 5;
-                }
-
-                DateRange(givenDate);
-            }
-            else
-            {
-                ErrorParameters();
-                return 1;
-            }
-
-            return 0;
         }
 
         /// <summary>
         /// Read the codes form the file given by the file parameter into codeAgents
         /// </summary>
         /// <param name="v"></param>
-        private static int ReadCodes(string filename)
+        public int ReadCodes(string filename)
         {
             // CHeck if filename is actual a file
             if (string.IsNullOrEmpty(filename) || !File.Exists(filename))
             {
-                Console.WriteLine("File " + filename + " does not exitst");
                 return 7;
             }
 
@@ -232,22 +107,22 @@ namespace OutlookCount
         /// <summary>
         /// Count the number of mails per agent in the defined range in the mailbox specified.
         /// </summary>
-        private static void CountMailbox()
+        private void CountMailbox()
         {
             // Get the MAPI namespace.
             Outlook.NameSpace NS = oApp.GetNamespace("Mapi");
 
             // Run over all mailboxes
             foreach (Folder folder in NS.Folders)
-                if (folder.Name == mailBox)
+                if (folder.Name == MailBox)
                 {
                     try
                     {
                         // Sent mails
-                        sentMailsCount(restrictDate, folder);
+                        sentMailsCount(RestrictDate, folder);
 
                         // Received mails
-                        ReceivedMailsCount(restrictDate, folder);
+                        ReceivedMailsCount(RestrictDate, folder);
 
                     }
                     //catch faulty stores
@@ -310,7 +185,7 @@ namespace OutlookCount
         /// Search in the mail for the first code of an agent found
         /// </summary>
         /// <param name="mailBody">body of the mail to search in</param>
-        private static void GetAgentFromMail(string mailBody)
+        private void GetAgentFromMail(string mailBody)
         {
             String Name = null;
             int Position = -1;
@@ -339,7 +214,23 @@ namespace OutlookCount
                 result.Amount++;
             }
         }
+
+        public List<String> readMailboxes()
+        {
+            var returnlist = new List<String>();
+
+            // Get the MAPI namespace.
+            Outlook.NameSpace NS = oApp.GetNamespace("Mapi");
+
+            // Run over all mailboxes
+            foreach (Folder folder in NS.Folders)
+            {
+                returnlist.Add(folder.Name);
+            }
+
+            return returnlist;
+        }
+
+
     }
 }
-
-
